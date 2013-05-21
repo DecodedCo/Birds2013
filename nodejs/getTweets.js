817fc766d5eb69f3b3c6e3dd5a6406a12e38d4bb
Ali Blackwell <aliblackwell@gmail.com>
2013-05-21 18:28:50 +0100

    Worked on nodejs script

--- nodejs/getTweets.js
+++ nodejs/getTweets.js
@@ -1,7 +1,8 @@
-var twitter = require('ntwitter');
+var Twitter = require('mtwitter');
 var fs = require('fs');
+var embedTweets = {};
 
-var twit = new twitter({
+var twitter = new Twitter({
   consumer_key: '05iXWPDYAWVlFGHeYRwtA',
   consumer_secret: 'alcd7nesaL1L1HxwcrYvCdK3CK5YH8fJjm1nQOjU',
   access_token_key: '18974419-FBAapWG8GBl7jDsScrc7QETX3id1aqlLSt2BKyvAz',
@@ -10,23 +11,46 @@ var twit = new twitter({
 
 var jsonObject = {};
 
+twitter.get('search/tweets',{q: 'birds2013'}, function(err, data) {
+    for (var i in data.statuses) {
+        //console.log(data.statuses[i].text);
+        var twitterID = data.statuses[i].id;
+        jsonObject[twitterID] = null;
+    }
+    var newData = JSON.stringify(jsonObject);
+    fs.writeFile('./data.json', newData, function(error){
+        if (error) { console.log(error) }
+        else { console.log('The file was saved') }
+    });
+});
 
-twit.search('birds2013 OR #birds2013 OR #birds2012 OR birds2012', {}, function(err, data) {
+// Read the data store
+fs.readFile('./data.json', function(error, json){
+    // Log any errors
+    if (error) { console.log(error) };
+    // Get the JSON contents and ensure it's JSON
 
-            for (var i=0; i<data.results.length; i++) {
+    var existingData = JSON.parse(json);
+   // console.log(existingData[0]);
+    // If already in list, increment checkinCount
 
-                console.log(data.results[i].id);
-                jsonObj.push({data.results[i].id: 'i'});
+    
 
-            }
-  
-            fs.writeFile('./data.json', data, function(error){
-                if (error) { console.log(error) }
-                else { console.log('The file was saved') }
-            });
-
+    for (var item in existingData) {
+        if(existingData.hasOwnProperty(item)) {
+             
+            twitter.get('statuses/oembed',{id: item},function(err,data){
+                embedTweets[item] = data;
+              
+            })
+        }
+    }
+    console.log(embedTweets);
 
 });
+
+
+
 
 
 
